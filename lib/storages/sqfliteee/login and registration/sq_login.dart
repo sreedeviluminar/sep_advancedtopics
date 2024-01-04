cb04528fc726a53140c0348b4f12f73edd3083ef
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-01-04 10:40:04 +0530

    initial commit

--- lib/storages/sqfliteee/login and registration/sq_login.dart
+++ lib/storages/sqfliteee/login and registration/sq_login.dart
@@ -2,50 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:sep_advancedtopics/storages/sqfliteee/login%20and%20registration/sq_registration.dart';
 
 class Sq_Login extends StatelessWidget {
+  var formkey     = GlobalKey<FormState>();
+  var email_cntrl = TextEditingController();
+  var pass_cntrl  = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Email"),
-                validator: (email) {
-                  if (email!.isEmpty || !email.contains("@")) {
-                    return "Invalid username or field is empty";
-                  }
-                },
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextFormField(
+                  controller: email_cntrl,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Email"),
+                  validator: (email) {
+                    if (email!.isEmpty || !email.contains("@")) {
+                      return "Invalid username or field is empty";
+                    }
+                  },
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Password"),
-                validator: (pass) {
-                  if (pass!.isEmpty || pass.length < 6) {
-                    return "Password length should be > 6 or field is empty";
-                  }
-                },
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextFormField(
+                  controller: pass_cntrl,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Password"),
+                  validator: (pass) {
+                    if (pass!.isEmpty || pass.length < 6) {
+                      return "Password length should be > 6 or field is empty";
+                    }
+                  },
+                ),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                child: Text("Login Now")),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => Sq_Register()));
-                },
-                child: Text("Register Here")),
-          ],
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login Now")),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => Sq_Register()));
+                  },
+                  child: Text("Register Here")),
+            ],
+          ),
         ),
       ),
     );
--- lib/storages/sqfliteee/login and registration/sq_registration.dart
+++ lib/storages/sqfliteee/login and registration/sq_registration.dart
@@ -1,57 +1,67 @@
 import 'package:flutter/material.dart';
 
 class Sq_Register extends StatelessWidget {
+  var formkey     = GlobalKey<FormState>();
+  var name_cntrl  = TextEditingController();
+  var email_cntrl = TextEditingController();
+  var pass_cntrl  = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                   border: OutlineInputBorder(),
-                  hintText: "Name"
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: name_cntrl,
+                  decoration: const InputDecoration(
+                     border: OutlineInputBorder(),
+                    hintText: "Name"
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Email"
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: email_cntrl,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Email"
+                  ),
+                  validator: (email){
+                    if(email!.isEmpty || !email.contains("@")){
+                      return "Invalid username or field is empty";
+                    }
+                  },
                 ),
-                validator: (email){
-                  if(email!.isEmpty || !email.contains("@")){
-                    return "Invalid username or field is empty";
-                  }
-                },
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Password"
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: pass_cntrl,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Password"
+                  ),
+                  validator: (pass){
+                    if(pass!.isEmpty || pass.length < 6){
+                      return "Password length should be > 6 or field is empty";
+                    }
+                  },
                 ),
-                validator: (pass){
-                  if(pass!.isEmpty || pass.length < 6){
-                    return "Password length should be > 6 or field is empty";
-                  }
-                },
               ),
-            ),
-            ElevatedButton(
-                onPressed: (){},
-                child: const Text("Register Now")
-            )
+              ElevatedButton(
+                  onPressed: (){},
+                  child: const Text("Register Now")
+              )
 
-          ],
+            ],
+          ),
         ),
       ),
     );
