d81e2c75a55b38b19ded26e4794e7af2827001c7
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-01-08 12:59:36 +0530

    initial commit

--- lib/storages/sqfliteee/login and registration/sq_user_home.dart
+++ lib/storages/sqfliteee/login and registration/sq_user_home.dart
@@ -1,45 +1,69 @@
 import 'package:flutter/material.dart';
 
-class UserHome extends StatelessWidget {
+class UserHome extends StatefulWidget {
   final data;
+
   UserHome({super.key, required this.data});
 
   @override
-  Widget build(BuildContext context) {
-    var name = data[0]['name'];
-    var cemail = TextEditingController();
-    var cname = TextEditingController();
+  State<UserHome> createState() => _UserHomeState();
+}
 
-    void editData(int id) {
+class _UserHomeState extends State<UserHome> {
+  @override
+  Widget build(BuildContext context) {
+    ///fetching the current user email and name 
+    var name   = widget.data[0]['name'];
+    var email  = widget.data[0]['email'];
+    
+    var cemail = TextEditingController();
+    var cname  = TextEditingController();
+
+    void editData() {
+      setState(() {
+        ///setting the current user details to the text fields inside alert box
+        cname.text  = name;
+        cemail.text = email;
+      });
+
       /// to show a dialogBox
       showDialog(
           context: context,
           builder: (context) {
             return AlertDialog(
-                title: const Text(
-                  "Edit Data",
-                  style: TextStyle(fontSize: 25),
-                ),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: cname,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(), hintText: "Edit Name"),
-                    ),
-                    SizedBox(height: 15,),
-                    TextField(
-                      controller: cemail,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(), hintText: "Edit Email"),
-                    ),
-                    SizedBox(height: 15,),
+              title: const Text(
+                "Edit Data",
+                style: TextStyle(fontSize: 25),
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: cname,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Edit Name"),
+                  ),
+                  SizedBox(height: 15,),
+                  TextField(
+                    controller: cemail,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Edit Email"),
+                  ),
+                  SizedBox(height: 15,),
 
-                    ElevatedButton(
-                        onPressed: () async {}, child: Text("Update Data"))
-                  ],
-                ),
+                  ElevatedButton(
+                      onPressed: (){
+                        setState(() {
+                          name = cname.text;
+                          email = cemail.text;
+                        });
+                        updateUser();
+                        Navigator.of(context).pop();
+                        cname.text ="";
+                        cemail.text="";
+                      }, child: Text("Update Data"))
+                ],
+              ),
               // actions: [
               //   TextButton(onPressed: (){}, child: Text('Yes')),
               //   TextButton(onPressed: (){}, child: Text('No')),
@@ -58,19 +82,21 @@ class UserHome extends StatelessWidget {
           child: ListTile(
             leading: const Icon(Icons.person_outline_outlined),
             title: Text(
-              data[0]['name'],
+              '$name',
               style: const TextStyle(fontSize: 20),
             ),
             subtitle: Text(
-              data[0]['email'],
+              '$email',
               style: const TextStyle(fontSize: 15),
             ),
             trailing: IconButton(
                 onPressed: () {
-                  editData(data[0]['id']);
+                  editData();
                 },
                 icon: Icon(Icons.edit)),
           ),
         ));
   }
+
+  void updateUser() {}
 }
