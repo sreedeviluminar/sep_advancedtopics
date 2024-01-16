82ebc194332fd3f0187b34a506ca29968eb49b07
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-01-16 13:11:06 +0530

    initial commit

--- lib/statemanagement/use_of_multi_provider/provider/auth_provider.dart
+++ lib/statemanagement/use_of_multi_provider/provider/auth_provider.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 class Auth_Provider extends ChangeNotifier {
   bool _loggedIn = false;
-
   bool get login => _loggedIn;
 
   void logedin() {
     _loggedIn = true;
     notifyListeners();
   }
-
   void logedOut() {
     _loggedIn = false;
     notifyListeners();
--- lib/statemanagement/use_of_multi_provider/view/using_multiprovider.dart
+++ lib/statemanagement/use_of_multi_provider/view/using_multiprovider.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sep_advancedtopics/statemanagement/use_of_multi_provider/provider/auth_provider.dart';
+import 'package:sep_advancedtopics/statemanagement/use_of_multi_provider/provider/counter_provider.dart';
 
-class Count_provider extends ChangeNotifier {
-  int _counter = 0;
+void main() {
+  runApp(MaterialApp(
+    home: MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Count_provider()),
+        ChangeNotifierProvider(create: (context) => Auth_Provider()),
+      ],
+      child: Multi_Providerr(),
+    ),
+  ));
+}
 
-  int get count => _counter;
-
-  void increment() {
-    _counter++;
-    notifyListeners();
+class Multi_Providerr extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Counter Value",
+              style: TextStyle(fontSize: 20),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Provider.of<Count_provider>(context, listen: false)
+                      .increment();
+                },
+                child: const Text("Increment Counter")),
+            Text(
+              "Count value :${Provider.of<Count_provider>(context).count}",
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Auth Details",
+              style: TextStyle(fontSize: 20),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Provider.of<Auth_Provider>(context).login
+                      ? Provider.of<Auth_Provider>(context).logedOut()
+                      : Provider.of<Auth_Provider>(context).logedin();
+                },
+                child: const Text("Switch Login")),
+            Consumer<Auth_Provider>(builder: (context, authprovider, child) {
+              return Text(
+                  "User is ${authprovider.login ? "Logged In" : "Logged Out"}");
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
