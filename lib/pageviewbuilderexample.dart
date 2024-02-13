9489362c1a9495e5b7ef883b9eb82e1819ed4d35
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-02-13 16:02:40 +0530

    initial commit

--- lib/pageviewbuilderexample.dart
+++ lib/pageviewbuilderexample.dart
@@ -18,31 +18,72 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyPageViewBuilder extends StatelessWidget {
-  final List<String> data = [
-    'Page 1',
-    'Page 2',
-    'Page 3',
-    'Page 4',
-    'Page 5',
-  ];
+class MyPageViewBuilder extends StatefulWidget {
+  @override
+  _MyPageViewBuilderState createState() => _MyPageViewBuilderState();
+}
+
+class _MyPageViewBuilderState extends State<MyPageViewBuilder> {
+  PageController _pageController = PageController();
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: data.length,
-      itemBuilder: (context, index) {
-        return buildPage(data[index]);
-      },
+    return Column(
+      children: [
+        Expanded(
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: 3, // Number of pages
+            itemBuilder: (context, index) {
+              return buildPage(index);
+            },
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                if (_currentPage > 0) {
+                  _pageController.previousPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                }
+              },
+              child: Text('Previous'),
+            ),
+            SizedBox(width: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                if (_currentPage < 2) {
+                  _pageController.nextPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                }
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
-  Widget buildPage(String content) {
+  Widget buildPage(int index) {
     return Container(
       color: Colors.blue,
       child: Center(
         child: Text(
-          content,
+          'Page ${index + 1}',
           style: TextStyle(
             fontSize: 24.0,
             color: Colors.white,
